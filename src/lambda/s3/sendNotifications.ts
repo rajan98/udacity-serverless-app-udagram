0cffaacc5331cbe8e3fbe4bc444aaa5b323eca77
rajan98 <rajan.kum.sha@gmail.com>
2020-06-07 18:59:24 +0530

    resizeImage implemented but getting an error SyntaxError: Cannot use import statement outside a module need to fix it

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,4 +1,4 @@
-import { S3Handler, S3Event } from 'aws-lambda'
+import { SNSHandler, SNSEvent, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 
@@ -16,24 +16,35 @@ const connectionParams = {
 
 const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams)
 
-export const handler: S3Handler = async (event: S3Event) => {
-    for(const record of event.Records){
-        const key = record.s3.object.key
-        console.log('Processing S3 item with key: ', key)
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  console.log('Processing SNS event ', JSON.stringify(event))
+  for (const snsRecord of event.Records) {
+    const s3EventStr = snsRecord.Sns.Message
+    console.log('Processing S3 event', s3EventStr)
+    const s3Event = JSON.parse(s3EventStr)
 
-        const connections = await docClient.scan({
-            TableName: connectionsTable
-        }).promise();
+    await processS3Event(s3Event)
+  }
+}
 
-        const payload = {
-            imageId: key
-        }
+async function processS3Event(s3Event: S3Event) {
+  for (const record of s3Event.Records) {
+    const key = record.s3.object.key
+    console.log('Processing S3 item with key: ', key)
 
-        for (const connection of connections.Items) {
-            const connectionId = connection.id
-            await sendMessageToClient(connectionId, payload)
-        }
+    const connections = await docClient.scan({
+      TableName: connectionsTable
+    }).promise();
+
+    const payload = {
+      imageId: key
     }
+
+    for (const connection of connections.Items) {
+      const connectionId = connection.id
+      await sendMessageToClient(connectionId, payload)
+    }
+  }
 }
 
 async function sendMessageToClient(connectionId, payload) {
